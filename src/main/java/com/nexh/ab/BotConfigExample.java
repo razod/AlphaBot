8ef396159ba182512c84dea99d88cabccda5ee1e
razod <masonboulier@gmail.com>
2020-05-23 14:22:31 -0400

    bot goes online

--- src/main/java/com/nexh/ab/BotConfigExample.java
+++ src/main/java/com/nexh/ab/BotConfigExample.java
@@ -1,5 +1,6 @@
 package com.nexh.ab;
 
 public class BotConfigExample {
+    // Change this class and file name and to BotConfig
     public static final String TOKEN = "token";
 }
--- src/main/java/com/nexh/ab/Main.java
+++ src/main/java/com/nexh/ab/Main.java
@@ -1,14 +1,14 @@
 package com.nexh.ab;
 
 import net.dv8tion.jda.api.JDABuilder;
-
 import javax.security.auth.login.LoginException;
+import com.nexh.ab.BotConfig;
 
 public class Main {
-    private Main() throws LoginException {
+    public static void main(String[] args) throws LoginException {
         new JDABuilder()
-                .setToken("")
-                .addEventListeners()
+                .setToken(BotConfig.TOKEN)
+                // .addEventListeners()
                 .build();
     }
 }
